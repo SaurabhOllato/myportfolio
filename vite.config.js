e342fcc7e83dfaacabc886685cd8620ec4a7fb30
SaurabhOllato <saurabh.ollato@gmail.com>
2025-06-16 17:28:31 +0530

    Deploying

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/myportfolio/', // <-- must match the repo name exactly
+  base: '/myportfolio/', // 👈 MUST match the repo name exactly
 });
