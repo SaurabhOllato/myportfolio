838d7226707eb86596c9e52a0e1add6af15e83e3
SaurabhOllato <saurabh.ollato@gmail.com>
2025-06-16 17:13:50 +0530

    baseline

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-   base: "/myportfolio/", // VERY IMPORTANT
+  base: '/myportfolio/', // <-- must match the repo name exactly
 });
