80ccf182a90d465414eb9d8e0fd9c919a8cb23a2
SaurabhOllato <saurabh.ollato@gmail.com>
2025-06-24 16:03:56 +0530

    vercel deployment try 2

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
       <React.StrictMode>
-    <BrowserRouter basename="/myportfolio"> {/* 👈 your base path here */}
+    <BrowserRouter basename="/"> 
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/myportfolio', // 👈 MUST match the repo name exactly
+  base: '/', // 👈 MUST match the repo name exactly
 });
